2c8c3bdfb0836b75ea508d81285f918d27cff9ac
Ubergeek <ubergeek@yourtilde.com>
2019-11-05 15:57:19 +0000

    Including tilde.json

---
+++ setenv.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Here is where you set config variables.
+
+export SITE_TITLE="Thunix"
+export SITE="https://thunix.net"
+export WANT_USERS="true"
+export ADMIN_EMAIL="root@thunix.net"
+export SIGNUP_URL="https://thunix.net/signup"
+export DESCRIPTION="Thunix is a community, centered around access to a public *nix system.  Thunix offers shell accounts with complete set of programming tools, and follows a continuous integration-continuous deployment of system configuration."
